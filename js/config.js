0dd5a982617edafc1dba46c75ca3f725dc71e9b6
ekufta0530 <eric.kufta@datadoghq.com>
2022-01-24 17:39:47 -0700

    invoke url added

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_oOFrQHbJu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4je6ogqm569d2q7ispu59d38g3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-2_oOFrQHbJu',
+        userPoolClientId: '4je6ogqm569d2q7ispu59d38g3',
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qfe2lj83i6.execute-api.us-east-2.amazonaws.com/prod',
     }
 };
